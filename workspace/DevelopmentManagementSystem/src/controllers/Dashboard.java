91e2c6043a6221433118ac864bcdf535ee9598ff
Arnav Singhania <arnavsinghania98@gmail.com>
2019-03-04 14:01:54 -0800

    Fixed security flaw that was letting people bypass the login page.

--- workspace/DevelopmentManagementSystem/src/controllers/Dashboard.java
+++ workspace/DevelopmentManagementSystem/src/controllers/Dashboard.java
@@ -1,20 +1,84 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Encrypter;
+
 
 @WebServlet("/Dashboard")
 public class Dashboard extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	public void init( ServletConfig config ) throws ServletException
+    {
+        super.init( config );
+
+        try
+        {
+            Class.forName( "com.mysql.jdbc.Driver" );
+        }
+        catch( ClassNotFoundException e )
+        {
+            throw new ServletException( e );
+        }
+    }
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/views/DashboardPage.jsp").forward(request, response);
-	}
+		
+		//confirm that the user is logged in
+		Connection c = null;
+	      try
+	      {
+	        String url = "jdbc:mysql://148.66.138.112:3306/projectX3337";
+	      	String username="Arnav";
+	      	String dbPassword="projectX3337";
+			String securedPassword = Encrypter.encrypt(request.getParameter("password"));
+	
+	          c = DriverManager.getConnection( url, username, dbPassword );
+	          Statement stmt = c.createStatement();
+		         
+	          String query = "select * from `users` WHERE email='"+request.getParameter("email")+"' AND password='"+securedPassword+"'";
+	          ResultSet rs = stmt.executeQuery( query );
+	
+	          if (rs.next()) {
+	        	  request.getRequestDispatcher("/WEB-INF/views/DashboardPage.jsp").forward(request, response);
+	          } else {
+	        	//if user is not logged in, redirect to the home page
+	        	  response.sendRedirect("Home");
+	          }
+	       
+	      }
+	      
+	      catch( SQLException e )
+	      {
+	          throw new ServletException( e );
+	      }
+	      finally
+	      {
+	          try
+	          {
+	              if( c != null ) c.close();
+	          }
+	          catch( SQLException e )
+	          {
+	              throw new ServletException( e );
+	          }
+	      }
+	 
+	      
+	} 
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
