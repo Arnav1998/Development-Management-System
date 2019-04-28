fbda74ae36fd2ba9623ea461aa20a712e01b5b0a
Arnav Singhania <arnavsinghania98@gmail.com>
2019-04-28 16:38:42 -0700

    Added basic files for Budget Analysis feature

--- workspace/DevelopmentManagementSystem/src/controllers/Budget.java
+++ workspace/DevelopmentManagementSystem/src/controllers/Budget.java
@@ -12,7 +12,7 @@ public class Budget extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		request.getRequestDispatcher("/WEB-INF/views/BudgetView.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
